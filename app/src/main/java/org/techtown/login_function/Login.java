eac29f32da29fc75b959d67e96b17fcbf37ad042
ludswdy03 <j031024y@naver.com>
2022-07-26 02:34:42 +0900

    login_func_practice

---
+++ app/src/main/java/org/techtown/login_function/Login.java
@@ -0,0 +1,15 @@
+package org.techtown.login_function;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
