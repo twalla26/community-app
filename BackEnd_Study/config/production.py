0589da832b8250543d115ce67446a63249a5facc
twalla <twalla@twallaui-MacBookAir.local>
2022-07-27 03:15:36 +0900

    update

--- BackEnd_Study/config/production.py
+++ BackEnd_Study/config/production.py
@@ -1,10 +1,5 @@
-import os
-
-
-BASE_DIR = os.path.dirname(__file__)
-
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-SECRET_KEY = "dev"
+SECRET_KEY = b'\x1elF\x11IH\x97\xdf\x94\xe4I\x8b\x83\xc03\xa5'
--- BackEnd_Study/pybo/models.py
+++ BackEnd_Study/pybo/models.py
@@ -6,6 +6,9 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable = False)
     content = db.Column(db.Text(), nullable = False)
     create_date = db.Column(db.DateTime(), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set')) 
+    modify_date = db.Column(db.DateTime(), nullable=True)
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -13,3 +16,13 @@ class Answer(db.Model):
     question = db.relationship("Question", backref = db.backref("answer_set", cascade = 'all, delete-orphan'))
     content = db.Column(db.Text(), nullable = False)
     create_date = db.Column(db.DateTime(), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('answer_set'))
+    modify_date = db.Column(db.DateTime(), nullable=True)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(150), unique = True, nullable = False)
+    password = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
