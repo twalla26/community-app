00b74e88f2df856baec81636d9d2ef0083787961
ludswdy03 <j031024y@naver.com>
2022-08-15 10:38:48 +0900

    App activity_board function

--- app/src/main/java/org/techtown/HowAboutThisDay/EditStudy.java
+++ app/src/main/java/org/techtown/HowAboutThisDay/EditStudy.java
@@ -2,13 +2,90 @@ package org.techtown.HowAboutThisDay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class EditStudy extends AppCompatActivity {
+
+    private TextView title_view, content_view;
+    private String title, content;
+    private static final String URL_Content_Study = "http://59.25.242.66:5000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_study);
+
+        title_view = findViewById(R.id.title_view);
+        content_view = findViewById(R.id.content_view);
+
+        Intent intent = getIntent();
+        Integer id = intent.getExtras().getInt("id");
+
+        send_request_Server_Edit_Study();
+        title_view.setText(title);
+        content_view.setText(content);
+    }
+    public void send_request_Server_Edit_Study() {
+
+        class sendData extends AsyncTask<Void, Void, String> {
+            @Override
+            protected void onPreExecute(){
+                super.onPreExecute();
+            }
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+            }
+            @Override
+            protected void onProgressUpdate(Void... values){
+                super.onProgressUpdate(values);
+            }
+            @Override
+            protected void onCancelled(String s){
+                super.onCancelled(s);
+            }
+            @Override
+            protected void onCancelled(){
+                super.onCancelled();
+            }
+            @Override
+            protected String doInBackground(Void... voids){
+                try {
+                    OkHttpClient client = new OkHttpClient();
+
+                    Request request = new Request.Builder()
+                            .url(URL_Content_Study)
+                            .build();
+                    Response responses = null;
+                    responses = client.newCall(request).execute();
+                    String response = responses.body().string();
+                    System.out.println(response);
+
+                    JSONObject jsonObject = new JSONObject(response);
+                    title = jsonObject.getString("title");
+                    content = jsonObject.getString("study_plan");
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+        sendData sendData = new sendData();
+        sendData.execute();
     }
 }
